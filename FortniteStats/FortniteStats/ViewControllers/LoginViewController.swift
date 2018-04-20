15a40186be7ccdb7fd5c08c14aad8ca85052c723
Jair Alejandro Pinedo Pacheco <jair.pin91@gmail.com>
2018-04-20 09:17:04 -0500

    link outles

--- FortniteStats/FortniteStats/ViewControllers/LoginViewController.swift
+++ FortniteStats/FortniteStats/ViewControllers/LoginViewController.swift
@@ -10,16 +10,17 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var nameTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
-
 
 }
 
