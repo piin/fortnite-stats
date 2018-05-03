bf47ec7bcd05bfb05174a59925843e8242f8d0f4
Jair Alejandro Pinedo Pacheco <jair.pin91@gmail.com>
2018-05-03 14:41:36 -0500

    Implement custom UISegmentedControl for selectedValue

---
+++ FortniteStats/FortniteStats/Customs/PlatformSegmentedControl.swift
@@ -0,0 +1,34 @@
+//
+//  PlatformSegmentedControl.swift
+//  FortniteStats
+//
+//  Created by Jair Pinedo on 03/05/18.
+//  Copyright © 2018 Jair Pinedo. All rights reserved.
+//
+
+import UIKit
+
+class PlatformSegmentedControl: UISegmentedControl {
+
+    enum Platforms: String {
+        case pc
+        case psn
+        case xbl
+    }
+    
+    /**
+     selected value, posible options: pc, psn & xbl. Default value pc
+     */
+    var selectedValue: Platforms {
+        
+        switch selectedSegmentIndex {
+        case 0: return .pc
+        case 1: return .psn
+        case 2: return .xbl
+        default :
+            return .pc
+        }
+        
+    }
+
+}
