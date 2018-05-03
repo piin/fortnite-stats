af26fdc010660c8fdf94d65b2fc786ad7e1d74c3
Jair Alejandro Pinedo Pacheco <jair.pin91@gmail.com>
2018-05-03 14:41:53 -0500

    Add HttpErrosModel

---
+++ FortniteStats/FortniteStats/Models/HttpErrosModel.swift
@@ -0,0 +1,15 @@
+//
+//  ErrorModel.swift
+//  FortniteStats
+//
+//  Created by Jair Pinedo on 03/05/18.
+//  Copyright © 2018 Jair Pinedo. All rights reserved.
+//
+
+import Foundation
+
+struct HttpErrosModel: Decodable {
+    
+    let error: String
+    
+}
