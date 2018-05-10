17ae8306fcabf213b29a3b8649b49409465bcf3f
Jair Alejandro Pinedo Pacheco <jair.pin91@gmail.com>
2018-05-10 14:19:13 -0500

    save user in DB.

--- FortniteStats/FortniteStats/ViewModels/LoginViewModel.swift
+++ FortniteStats/FortniteStats/ViewModels/LoginViewModel.swift
@@ -8,52 +8,44 @@
 
 import Foundation
 
-class LoginViewModel {
-    
+enum LoginErrors : String {
+    case invalidUserName = "Invalid Fortnite name"
+}
+
+class LoginViewModel: ILoginViewModel {
+
     // MARK: - Properties
     
     var userModel: UserModel?
-    private var validUserName = false
-    private var repository = LoginRepository()
+    private var repository: LoginRepository
     
-    enum LoginErrors : String {
-        case invalidUserName = "Invalid Fortnite name"
+    init() {
+        repository = LoginRepository()
     }
-    
+
     // MARK: - Methods
     
-    /**
-     Validate if userName is not nil and has more than 1 character
-     - parameters:
-        - userName: String? with the userName
-     - returns: LoginErrors?
-     */
     func validateUserName(userName: String?) -> LoginErrors? {
         guard let text = userName, text.count > 1 else {
-            validUserName = false
             return .invalidUserName
         }
-        validUserName = true
         return nil
     }
     
-    /**
-     Make login request
-     - parameters:
-        - platform: String can be pc, xbl, psn
-        - userName: String
-        - completionHandler: result closure with userModel or error
-     */
     func login(userName: String, platform: String, completionHandler: @escaping (Result<UserModel>) -> ()) {
         
         repository.login(userName: userName, platform: platform) { (result) in
+            
             switch result {
             case .Success(let user):
                 self.userModel = user
+                self.repository.saveUser(userModel: user)
             case .Failure(let error):
                 print(error.getErrorMessage())
             }
+            // After make all the logic return the result enum
             completionHandler(result)
+            
         }
 
     }
