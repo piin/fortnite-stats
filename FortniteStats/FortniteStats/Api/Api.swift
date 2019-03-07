4c82513f7583637556b78830edea0caa8f8e530d
Jair Pinedo <jair.pinedo@unosquare.com>
2019-03-07 12:52:11 -0600

    Add constants for http headers

--- FortniteStats/FortniteStats/Api/Api.swift
+++ FortniteStats/FortniteStats/Api/Api.swift
@@ -11,5 +11,7 @@ import Foundation
 struct Api {
     
     static let BASE_URL = "https://api.fortnitetracker.com/v1/profile"
+    static let HEADER_KEY = "TRN-Api-Key"
+    static let HEADER_KEY_VALUE = "55a19837-9d24-46bf-81fe-6ba01bd40efd"
     
 }
