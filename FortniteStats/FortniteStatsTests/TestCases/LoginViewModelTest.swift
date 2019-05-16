81585123712470eeea31cfa527b10b7dcb140016
piin <jair.pin91@gmail.com>
2019-05-16 12:44:28 -0500

    Remove useless files

--- FortniteStats/FortniteStatsTests/TestCases/LoginViewModelTest.swift
+++ FortniteStats/FortniteStatsTests/TestCases/LoginViewModelTest.swift
@@ -7,46 +7,17 @@
 //
 
 import XCTest
-@testable import FortniteStats
+
 
 class FortniteStatsTests: XCTestCase {
     
-    private var viewModel: LoginViewModel!
-    private var repository: LoginRepositoryMock!
-    
-    private var validUserName = "piin"
-    private var platform = "pc"
-    private var invalidUserName = "p"
-    
     override func setUp() {
         super.setUp()
-        repository = LoginRepositoryMock()
-        viewModel = LoginViewModel(repository: repository)
+
     }
     
     override func tearDown() {
         super.tearDown()
-    }
-    
-    func testValidateUserName() {
-        let areErrors = viewModel.validateUserName(userName: validUserName)
-        XCTAssertNil(areErrors, "Invalid Fortnite name")
-    }
-    
-    func testInvalidUserName() {
-        let areErrors = viewModel.validateUserName(userName: invalidUserName)
-        XCTAssertNotNil(areErrors, "Valid Fortnite name")
-    }
-
-    func testLogin() {
-        viewModel.login(userName: validUserName, platform: platform) { (result) in
-            switch result {
-            case .Success(let user):
-                XCTAssertNotNil(user, "Error with the user")
-            case .Failure(let error):
-                 XCTAssertNil(error, "Error with the error")
-            }
-        }
     }
     
 }
